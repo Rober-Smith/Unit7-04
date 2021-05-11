896656643ab9c43bdc937e617a9589069a8d55b4
RoberSmith <>
2021-05-11 14:57:29 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById('button').addEventListener('click',compare)
+let date = 0
+let age = 0
+
+function compare() {
+  age = document.getElementById('input').value
+  age =  parseInt(age)
+  date = document.getElementById('input2').value
+  date =  parseInt(date)
+
+  if (date == "tuesday" || date == "thursday"||age > 12 && age < 25) {
+  userInput = document.getElementById('answer').innerHTML = "You get the discount"
+  } else {
+  userInput = document.getElementById('answer').innerHTML = "you must pay regular price"
+  }
+}
