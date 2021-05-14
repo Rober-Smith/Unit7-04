2b3bbd76e0d70fd29c0c39c31f92c845f9ae5c80
RoberSmith <>
2021-05-14 18:32:34 +0000

    owo

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-document.getElementById('button').addEventListener('click',compare)
+document.getElementById('button').addEventListener('click', compare)
 let date = 0
 let age = 0
 
-function compare() {
+function compare () {
   age = document.getElementById('input').value
-  age =  parseInt(age)
+  age = parseInt(age)
   date = document.getElementById('input2').value
-  date =  parseInt(date)
+  date = parseInt(date)
 
-  if (date == "tuesday" || date == "thursday"||age > 12 && age < 25) {
-  userInput = document.getElementById('answer').innerHTML = "You get the discount"
-  } else {
-  userInput = document.getElementById('answer').innerHTML = "you must pay regular price"
+  if (date === 'tuesday' || date === 'thursday' && age > 12 && age < 25) {
+    date = document.getElementById('answer').innerHTML = 
+  'You get the discount'
+    } else {
+    date = document.getElementById('answer').innerHTML = 
+  'you must pay regular price'
   }
 }
